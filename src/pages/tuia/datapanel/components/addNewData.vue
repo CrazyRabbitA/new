bcc5175989ab21d28ddd9d36cd58d4592c71f378
crazyrabbit <1210454611@qq.com>
2018-09-12 11:39:26 +0800

    重新处理数据

--- src/pages/tuia/datapanel/components/addNewData.vue
+++ src/pages/tuia/datapanel/components/addNewData.vue
@@ -10,29 +10,134 @@
       <table class="recommend-list mt10">
         <thead class="header">
           <tr class="tr-header">
-            <template v-for="(val, key) in mapArr[0]">
-              <th :key="index">
-                <label class="trade-warp">
-                  {{val|translate}}
+            <template v-for="(index, item) in columns">
+              <th :key=" index ">
+                <label class="trade-warp ">
+                  {{item.title}}
                 </label>
               </th>
             </template>
           </tr>
         </thead>
-
         <tbody>
-          <tr class="tr-items" v-for="(item,indexs) in mapArr">
-            <td v-for="(item,index) in indexs ">{{index|fixNum}}</td>
+          <tr class="tr-items " v-for="(index,items) in mapArr ">
+            <template v-for="column in columns">
+              <td v-if="column.render">
+                {{column.render(items[column.key])}}
+              </td>
+              <td v-else>
+                <span v-if="column.key === 'id'">
+                  <a>{{items.id}}</a>
+                </span>
+                <span v-else>
+                  {{items[column.key]}}
+                </span>
+              </td>
+            </template>
           </tr>
         </tbody>
       </table>
-      <!-- <tableempty v-if="!showTable"></tableempty> -->
+      <!-- <tableempty v-if="!showTable "></tableempty> -->
     </div>
   </div>
 </template>
 <script>
-import { _fixNum } from 'components/utils/cheatDataUtils';
+import { _fixNum, getPrecent } from 'components/utils/cheatDataUtils';
 import MessageBox from 'components/basic/MessageBox';
+let columns = [
+  {
+    title: '媒体ID',
+    key: 'id',
+    fixed: 'left',
+    width: 80
+  },
+  {
+    title: '媒体名称',
+    key: 'appName',
+    fixed: 'left',
+    width: 100
+  },
+  {
+    title: '分析时段消耗',
+    key: 'currentValue',
+    width: 110
+  },
+  {
+    title: '对比时段消耗',
+    key: 'contrastValue',
+    width: 110
+  },
+  {
+    title: '变化值',
+    key: 'variation',
+    width: 100
+  },
+  {
+    title: '占比',
+    key: 'proportion',
+    width: 100,
+    tooltip: true,
+    content: '媒体消耗变化值/大盘消耗变化值',
+    render(row, column) {
+      return getPrecent(row);
+    }
+  },
+  {
+    title: '广告位访问UV影响系数',
+    key: 'sdkUvAfModulus',
+    width: 120,
+    render(row, column) {
+      if (row === 'NaN') {
+        return '-';
+      }
+      return getPrecent(row);
+    }
+  },
+  {
+    title: '每UV券请求影响系数',
+    key: 'arcPsuAfModulus',
+    width: 115,
+    render(row, column, index) {
+      if (row === 'NaN') {
+        return '-';
+      }
+      return getPrecent(row);
+    }
+  },
+  {
+    title: '发券成功率影响系数',
+    key: 'lcParcAfModulus',
+    width: 105,
+    render(row, column, index) {
+      if (row === 'NaN') {
+        return '-';
+      }
+      return getPrecent(row);
+    }
+  },
+  {
+    title: '付费券占比影响系数',
+    key: 'plpPlcAfModulus',
+    width: 105,
+    render(row, column, index) {
+      if (row === 'NaN') {
+        return '-';
+      }
+      return getPrecent(row);
+    }
+  },
+  {
+    title: '付费券arpu值影响系数',
+    key: 'acPplpAfModulus',
+    width: 120,
+    render(row, column, index) {
+      if (row === 'NaN') {
+        return '-';
+      }
+      return getPrecent(row);
+    }
+  }
+];
 export default {
   ready() {
     this.getData();
@@ -40,12 +145,22 @@ export default {
   data() {
     return {
       data: {},
-      mapArr: []
+      mapArr: [],
+      columns
     };
   },
   filters: {
     fixNum(data) {
       if (typeof data === 'number') {
+        let stringData = String(data);
+        if (stringData.indexOf('.') === -1) {
+          return data;
+        }
+        let arr = stringData.split('.');
+        if (arr[1].toString().length < 4) {
+          return data;
+        }
+
         return _fixNum(data, 4);
       } else if (data === 'NaN') {
         return '-';
